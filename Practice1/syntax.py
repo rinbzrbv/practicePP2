23f7b27554fc4c5cb09bb9a48f02b6ae45f85681
Rinat <rinat.bazarbayev07@gmail.com>
2026-01-26 23:56:01 +0500

    описание изменений

---
+++ Practice1/syntax.py
@@ -0,0 +1,15 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
